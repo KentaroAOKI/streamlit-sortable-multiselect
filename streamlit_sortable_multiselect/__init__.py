d4563d2c1789f8afc4790b21280ba80dad05f64b
Kentaro Aoki <kentaro.aoki@gmail.com>
2026-05-31 00:18:43 +0000

    version 0.7.3

--- streamlit_sortable_multiselect/__init__.py
+++ streamlit_sortable_multiselect/__init__.py
@@ -8,7 +8,7 @@ from typing import Any, Iterable, Mapping, Sequence, cast
 
 import streamlit.components.v1 as components
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 __all__ = ["sortable_multiselect"]
 
 _COMPONENT_NAME = "streamlit_sortable_multiselect"
